6cf183f7118d710d32d1bf53633672a4560a58d4
aaryashaji1 <aaryashajit@gmail.com>
2021-03-22 16:04:06 -0500

    Add project files.

---
+++ MVCNavigation/Controllers/MenuController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCNavigation.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Service()
+        {
+            return View();
+        }
+        public ActionResult Products()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult LoginPage()
+        {
+            return View();
+        }
+
+
+
+    }
+}
